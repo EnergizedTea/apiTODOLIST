007df7cd2b00317916805f4befdd76a634ac66cc
Diego Pacheco <diego.pacheco.valdez@gmail.com>
2024-03-25 16:03:08 -0600

    Solved the issue regarding tasks.db, finished the create task function using method post.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 
@@ -14,15 +15,20 @@ class Task(db.Model):
     category = db.Column(db.String(120), nullable=False)
     status = db.Column(db.Boolean, default=False)
 
-    # Representación
-    def _repr_(self):        
-        return f'<Task {self.name} under {self.category} with status {self.status}>'
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'category': self.category,
+            'status': self.status
+        }
+    
 
 @app.route('/')
 def home():
     return 'Welcome to my To-Do List'
 
-@app.route('/create')
+@app.route('/create', methods=['POST'])
 def create_task():
     if not request.json:
         abort(400, error = 'Missing body in request')
@@ -30,13 +36,20 @@ def create_task():
         return jsonify({"ERROR":"Missing name"}), 400
     if 'category' not in request.json:
         return jsonify({"ERROR":"Missing category"}), 400
+    
     task = Task(name=(request.json['name']), category=(request.json['category']), status=False)
     db.session.add(task)
     db.session.commit()
-    return jsonify({'Task added!'}), 201
+    
+    return jsonify(task.to_dict()), 201
+
 
 
 # THIS RIGHT HERE IS THE REASON OUR APP RUNS LET'S GOOOOOOOOOOOOOOOOOOOOOO
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        print("Tables created...")
+
     # YOU BETTER MAKE SURE DEBUG IS SET TO FALSE WHEN YOU DEPLOY BOI
     app.run(debug=True)
