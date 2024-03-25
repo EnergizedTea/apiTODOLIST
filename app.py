6949fd5d7749c1a0c0fd70b538a1f7812aeaf2e3
Diego Pacheco <diego.pacheco.valdez@gmail.com>
2024-03-25 17:52:49 -0600

    Finished creation of CRUD, moving onto using a render db.

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask, jsonify, request, abort
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func
-from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
+BASE_URL = '/api/v1/'
 
 class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(120), nullable=False)
     category = db.Column(db.String(120), nullable=False)
     status = db.Column(db.Boolean, default=False)
 
-    def to_dict(self):
+
+    def show(self):
         return {
             'id': self.id,
             'name': self.name,
@@ -28,23 +28,79 @@ class Task(db.Model):
 def home():
     return 'Welcome to my To-Do List'
 
-@app.route('/create', methods=['POST'])
-def create_task():
+@app.route(BASE_URL + 'create', methods=['POST'])
+def createTask():
     if not request.json:
         abort(400, error = 'Missing body in request')
     if 'name' not in request.json:
-        return jsonify({"ERROR":"Missing name"}), 400
+        abort(400, error = 'Missing name in request')
     if 'category' not in request.json:
-        return jsonify({"ERROR":"Missing category"}), 400
+        abort(400, error = 'Missing category in request')
     
     task = Task(name=(request.json['name']), category=(request.json['category']), status=False)
     db.session.add(task)
     db.session.commit()
     
-    return jsonify(task.to_dict()), 201
+    return jsonify({'Created task succesfully!': task.show()}), 201
+
+@app.route(BASE_URL + 'create/<int:id>', methods=['POST'])
+def createTaskWithId(id):
+    if not request.json:
+        abort(400, error = 'Missing body in request')
+    if 'name' not in request.json:
+        abort(400, error = 'Missing name in request')
+    if 'category' not in request.json:
+        abort(400, error = 'Missing category in request')
+    if Task.query.get(id) is None:
+        task = Task(id=id,name=(request.json['name']), category=(request.json['category']), status=False)
+        db.session.add(task)
+        db.session.commit()
+        
+        return jsonify({'Created task succesfully!': task.show()}), 201
+    else:
+        return jsonify({'Id already in existace'}), 409
+
+@app.route(BASE_URL + 'change/<int:id>', methods=['PATCH'])
+def updateTask(id):
+    task = Task.query.get(id)
+    if task is None:
+        abort(404, error="Task not found!")
+        
+    if 'name' and 'category' not in request.json:
+        abort(400, error = 'No body, nothing to change')
+        
+    if 'name' in request.json:
+        task.name = request.json['name']
+    if 'category'in request.json:
+        task.category = request.json['category']
+        
+    db.session.commit()
+    return jsonify('Task has been changed has been succesfully changed'), 200
+    
+@app.route(BASE_URL + 'tasks', methods=['GET'])
+def getTasks():
+    allTasks = Task.query.all()
+    return jsonify({'These are all your tasks': [task.show() for task in allTasks]}), 200
+
+@app.route(BASE_URL + 'tasks/<int:id>', methods=['GET'])
+def getTask(id):
+    task = Task.query.get(id)
+    if task is None:
+        abort(404, error="Task not found!")
+    return jsonify({'This is the task you asked for': task.show()}), 200
+
+@app.route(BASE_URL + 'tasks/<int:id>', methods=['DELETE'])
+def deleteTask(id):
+    task = Task.query.get(id)
+    if task is None:
+        abort(404, error="Task not found!")
+    db.session.delete(task)
+    db.session.commit()
+    
+    return jsonify('Task has been deleted'), 200
 
 
-
+    
 # THIS RIGHT HERE IS THE REASON OUR APP RUNS LET'S GOOOOOOOOOOOOOOOOOOOOOO
 if __name__ == "__main__":
     with app.app_context():
